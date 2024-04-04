d08df26fe6fdf6e798572826215c2c6d4df47b33
oi <zedokapakapapiscoulevou@gmail.com>
2024-04-04 11:21:44 -0300

    Projeto limpo

---
+++ Nao/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Nao;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Nao/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Nao;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
