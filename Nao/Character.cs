dbda908a44fbc5c7bca625cbca64b0d48b05e035
oi <zedokapakapapiscoulevou@gmail.com>
2024-04-04 11:23:39 -0300

    Backup do Projeto

---
+++ Nao/Character.cs
@@ -0,0 +1,86 @@
+namespace Nao;
+
+
+        public class Personagem
+{
+
+
+
+
+
+    public int mewingLevel;
+
+    public int sedeLevel;
+
+    public int fomeLevel;
+
+
+
+
+
+        public Personagem()
+
+{
+    mewingLevel = 0;
+    sedeLevel = 0;
+    fomeLevel = 0;
+}
+
+
+//-----------------------------------------------------------------------------------------------------
+
+
+    public void SetFome (int f)
+    {
+        
+        if (1 <= f || f >= 0)
+          
+          mewingLevel = f;
+
+        else if (f > 1)
+
+          mewingLevel = 1;
+
+        else
+
+          mewingLevel = 0;
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
