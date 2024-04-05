106043cad2b149dd754094981311be46dc0bc493
Caio <zedokapakapapiscoulevou@gmail.com>
2024-04-05 09:02:59 -0300

    Passos finais do character.cs

--- Nao/Character.cs
+++ Nao/Character.cs
@@ -1,62 +1,84 @@
 namespace Nao;
 
 
-        public class Personagem
+  public class Personagem
 {
+    double Mewing;
+    double Sede;
+    double Fome;
+
+
+
+  public Personagem()
+  
+  {
+    
+    Mewing = 0;
+
+    Sede = 0;
+
+    Fome = 0;
+    
+  
+  }
+
+
+
+
+  //-------------------------------------------
 
 
 
 
 
-    public int mewingLevel;
-
-    public int sedeLevel;
-
-    public int fomeLevel;
-
-
-
-
-
-        public Personagem()
-
-{
-    mewingLevel = 0;
-    sedeLevel = 0;
-    fomeLevel = 0;
-}
-
-
-//-----------------------------------------------------------------------------------------------------
-
-
-    public void SetFome (int f)
-    {
+  public void SetMewing (double m)
+  {
         
-        if (1 <= f || f >= 0)
-          
-          mewingLevel = f;
+    if (m < 0)
+        Mewing = 0;
 
-        else if (f > 1)
+    else if (m > 1)
+        Mewing = 1;
 
-          mewingLevel = 1;
+    else
+        Mewing = m;
 
-        else
-
-          mewingLevel = 0;
-
-    }
+  }
 
 
 
+  public void SetSede (double s)
+  {
+        
+    if (s < 0)
+        Sede = 0;
+
+    else if (s > 1)
+        Sede = 1;
+
+    else
+        Sede = s;
+
+  }
 
 
 
+  public void SetFome (double f)
+  {
+        
+    if (f < 0)
+        Mewing = 0;
+
+    else if (f > 1)
+        Mewing = 1;
+
+    else
+        Mewing = f;
+
+  }
 
 
-
-
-
+//-------------------------------------------
 
 
 
